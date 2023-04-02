ba93621049f773117b121b32d9d21132e4e69244
elwafa <eslamaboelwafa@gmail.com>
2023-04-02 04:47:48 +0200

    reset configs

--- config/laravel-request-tracker.php
+++ config/laravel-request-tracker.php
@@ -12,25 +12,25 @@ return [
      * The main Project name to be identified with the logging service
      *
      * */
-    'main_project_name' => env('LARAVEL_REQUEST_TRACKER_MAIN_PROJECT_NAME', 'ouredu'),
+    'main_project_name' => env('LARAVEL_REQUEST_TRACKER_MAIN_PROJECT_NAME', 'laravel-request-tracker'),
 
     /*
      * The sub name of the project and should be identified with the logging service
      *
      * */
-    'project_name' => env('LARAVEL_REQUEST_TRACKER_PROJECT_NAME', 'dashboard'),
+    'project_name' => env('LARAVEL_REQUEST_TRACKER_PROJECT_NAME', 'laravel-request-tracker'),
 
     /*
      * The API KEY to send the request to the logging service
      *
      * */
-    'api_key' => env('LARAVEL_REQUEST_TRACKER_API_KEY', 'API_KEY_2'),
+    'api_key' => env('LARAVEL_REQUEST_TRACKER_API_KEY', 'laravel-request-tracker'),
 
     /*
      * The URL to send the request to the logging service
      *
      * */
-    'url' => env('LARAVEL_REQUEST_TRACKER_URL', 'http://localhost:5002/api/v1/http-logger/'),
+    'url' => env('LARAVEL_REQUEST_TRACKER_URL', 'http://localhost:8000/api/v1/tracker'),
 
     /*
      * The Identification name with merged with request data to be identified with the logging service in response
